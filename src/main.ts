7af1448d2fc68ecc11a22ba53daa0a46ca6b079f
nilesh jadhav <meetnileshjadhav@gmail.com>
2025-05-06 17:30:50 +0530

    Addded logger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cors from 'cors';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 // const corsOptions : cors.CorsOptions = {
 //   origin : 'http://localhost:3001'
@@ -10,21 +11,26 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
   // app.use(cors(corsOptions));
 
   const configService = app.get(ConfigService);
 
   // Swagger setup
   const config = new DocumentBuilder()
-  .setTitle('AI Exam 5 May API')
-  .setDescription('API documentation for the AI Exam 5 May project')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('AI Exam 5 May API')
+    .setDescription('API documentation for the AI Exam 5 May project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen(configService.get<number>('PORT') || 3000);
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
